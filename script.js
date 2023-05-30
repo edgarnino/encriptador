0b119aefcd97df1f4e349c3275d43b184371c207
Edgar Solis SITE <edgar_antonio22@hotmail.com>
2023-05-30 11:16:55 -0600

    Se agregaron las funciones de encriptar, desencriptar y copiar

---
+++ script.js
@@ -0,0 +1,68 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const copiar = document.querySelector(".btn-copiar");
+copiar.style.display = "none";
+
+function validar() {
+    let textoEscrito = document.querySelector(".text-area").value;
+    let validador = textoEscrito.match(/^[a-z]*$/);
+
+    if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos");
+        location.reload();
+        return true;
+    }
+}
+
+function btnEncriptar() {
+    if(!validar()) {
+        const textoEncriptado = encriptar(textArea.value);
+        mensaje.value = textoEncriptado;
+        mensaje.style.backgroundImage = "none";
+        textArea.value = "";
+        copiar.style.display = "block";
+    }
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea = "";
+}
+
+
+//  CLAVE 
+// `La letra "e" es convertida para "enter"`
+// `La letra "i" es convertida para "imes"`
+// `La letra "a" es convertida para "ai"`
+// `La letra "o" es convertida para "ober"`
+// `La letra "u" es convertida para "ufat"`
+
+function encriptar(stringEncriptar) {
+    let claves = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptar = stringEncriptar.toLowerCase();
+
+    for(i=0; i<claves.length; i++) {
+        if(stringEncriptar.includes(claves[i][0])) {
+        stringEncriptar = stringEncriptar.replaceAll(claves[i][0], claves[i][1]);
+        }
+    } return stringEncriptar;
+}
+
+function desencriptar(stringDesencriptar) {
+    let claves = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptar = stringDesencriptar.toLowerCase();
+
+    for(i=0; i<claves.length; i++) {
+        if(stringDesencriptar.includes(claves[i][0])) {
+            stringDesencriptar = stringDesencriptar.replaceAll(claves[i][1], claves[i][0]);
+        }
+    } return stringEncriptar;    
+}
+
+function copiarTexto() {
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value);
+    mensaje.value = "";
+    alert("Texto copiado");
+}
